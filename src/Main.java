e015bee5134df050a28ca46df6a3b23a651c8bcc
marina <greenbox22@yandex.ru>
2023-09-21 02:21:43 +0400

    0

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+}
+Scanner sc = new Scanner(System.in);
+    public static String calc(String input){
+
+    return input;
+    }
+
+}
