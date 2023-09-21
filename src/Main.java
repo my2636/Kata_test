a6d36c7fcd0845323dcdc0ace4de096f0dabac00
marina <greenbox22@yandex.ru>
2023-09-21 04:17:44 +0400

    1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        String input = new Scanner(System.in).nextLine();
+        calc(input);
 }
-Scanner sc = new Scanner(System.in);
-    public static String calc(String input){
 
+    public static String calc(String input){
+        String[] roman = {"X","I","II","III","IV","V","VI","VII","VIII","IX"};
+        int[] arabic = new int[10];
+        for(int i=0; i<arabic.length; i++) {
+            arabic[i] = i+1;
+        }
+        System.out.println(Arrays.toString(arabic));
     return input;
     }
 
