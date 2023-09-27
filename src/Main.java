d01607c7af4fb37778eaa30d434cfee21a5ab290
marina <greenbox22@yandex.ru>
2023-09-28 03:55:30 +0400

    3

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,94 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String input = new Scanner(System.in).nextLine();
-        calc(input);
+        if (input.matches("\\+|-|\\*|/")){
+            System.out.println(calc(input));
+        } else {
+            System.out.println("(");
+        }
     }
 
     public static String calc(String input) {
         String[] nums = input.split("\\+|-|\\*|/");
-        if(input.contains("+")){
-            System.out.println(Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]));
-        } else if(input.contains("-")){
-            System.out.println(Integer.parseInt(nums[0]) - Integer.parseInt(nums[1]));
-        } else if(input.contains("*")){
-            System.out.println(Integer.parseInt(nums[0]) * Integer.parseInt(nums[1]));
-        } else if(input.contains("/")) {
-            System.out.println(Integer.parseInt(nums[0]) / Integer.parseInt(nums[1]));
+        int a = 0;
+        try{
+           int c = getArabic(nums[0]);
+           int d = getArabic(nums[1]);
+            if(c>0&&c<11&&d>0&&d<11){
+                switch (input){
+                    case "+":
+                        a = c+d;
+                        break;
+                    case "-":
+                        a = c-d;
+                        break;
+                    case "*":
+                        a = c*d;
+                        break;
+                    case "/":
+                        a = c/d;
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                System.out.println("(((");
+            }
+        } catch (Exception e) {
+            System.out.println("(");
         }
-        return input;
+
+        try {
+            int c = Integer.parseInt(nums[0]);
+            int d = Integer.parseInt(nums[1]);
+            if(c>0&&c<11&&d>0&&d<11){
+                switch (input){
+                    case "+":
+                        a = c+d;
+                        break;
+                    case "-":
+                        a = c-d;
+                        break;
+                    case "*":
+                        a = c*d;
+                        break;
+                    case "/":
+                        a = c/d;
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                System.out.println("(((");
+            }
+        } catch (Exception f) {
+            System.out.println("((");
+        }
+        return Integer.toString(a);
     }
+    static int getArabic(String a){
+        enum Roman {
+            I(1),
+            II(2),
+            III(3),
+            IV(4),
+            V(5),
+            VI(6),
+            VII(7),
+            VIII(8),
+            IX(9),
+            X(10),
+            L(50),
+            C(100);
 
-
-
-
+            private int lable;
+            Roman(int s) {lable = s;}
+            public int getLable() {return lable;}
+        }
+            int r = Roman.valueOf(a).getLable();
+            System.out.println(r);
+        return r;
     }
-    enum roman {
-        I(1),
-        II(2),
-        III(3),
-        IV(4),
-        V(5),
-        VI(6),
-        VII(7),
-        VIII(8),
-        IX(9),
-        X(10),
-        L(50),
-        C(100);
-
-        private int lable;
-        roman(int s) {lable = s;}
-        public int getLable() {return lable;}
-    }
-
-//String[] roman = {"X", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-//String[] signs = {"+", "-", "*", "/"};
-//int i1 = input.indexOf("+");
+}
