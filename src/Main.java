aae2f3a7183143f4011491fe76de55e8e10fa713
marina <greenbox22@yandex.ru>
2023-10-03 01:02:10 +0400

    5

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,26 @@ public class Main {
     public static void main(String[] args) {
         String input = new Scanner(System.in).nextLine();
         int count = 0;
+        String operator = "";
         for(char c: input.toCharArray()) {
             if (Character.isDigit(c)) {
                 count++;
+            } else if (c == '+') {
+                operator = "+";
+            } else if (c == '-') {
+                operator = "-";
+            } else if (c == '*') {
+                operator = "*";
+            } else if (c == '/') {
+                operator = "/";
             }
         }
 
         if(count>0) {
             System.out.println(calc(input));
-        } else {
+        } else if (count==0) {
             try {
-                String[] nums = getArabic(input);
+                System.out.println(calc(getArabic(input, operator)));
             } catch (Exception e) {
                 System.out.println("Exception 1");
             }
@@ -40,29 +49,27 @@ public class Main {
         return a;
     }
 
-    static String[] getArabic(String input) {
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("I",1);
-        map.put("II", 2);
-        map.put("III", 3);
-        map.put("IV", 4);
-        map.put("V", 5);
-        map.put("VI", 6);
-        map.put("VII", 7);
-        map.put("VIII", 8);
-        map.put("IX", 9);
-        map.put("X", 10);
+    static String getArabic(String input, String operator) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("I","1");
+        map.put("II", "2");
+        map.put("III", "3");
+        map.put("IV", "4");
+        map.put("V", "5");
+        map.put("VI", "6");
+        map.put("VII", "7");
+        map.put("VIII", "8");
+        map.put("IX", "9");
+        map.put("X", "10");
         String[] nums = input.split("\\+|-|\\*|/");
+        String c = "";
         try {
-            String a = map.get(nums[0]).toString();
-            String b = map.get(nums[1]).toString();
-            nums[0] = a;
-            nums[1] = b;
-            System.out.println(Arrays.toString(nums));
+            String a = map.get(nums[0]);
+            String b = map.get(nums[1]);
+            c = a + operator + b;
         } catch ( NoSuchElementException e) {
             System.out.println("exception (");
         }
-        System.out.println(Arrays.toString(nums));
-        return nums;
+        return c;
     }
 }
